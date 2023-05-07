1f130b0d64c312b80761a3ac028f497b4bf1eee4
chenshaorui <617672431@qq.com>
2023-05-07 21:37:38 +0800

    feat(problem-21): add solution 1

---
+++ Problem-21/Solution-1/main_test.go
@@ -0,0 +1,92 @@
+package Solution_1
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_mergeTwoLists(t *testing.T) {
+	type args struct {
+		list1 *ListNode
+		list2 *ListNode
+	}
+	tests := []struct {
+		name string
+		args args
+		want *ListNode
+	}{
+		{
+			name: "Test",
+			args: args{
+				list1: &ListNode{
+					Val: 1,
+					Next: &ListNode{
+						Val: 2,
+						Next: &ListNode{
+							Val:  4,
+							Next: nil,
+						},
+					},
+				},
+				list2: &ListNode{
+					Val: 1,
+					Next: &ListNode{
+						Val: 3,
+						Next: &ListNode{
+							Val:  4,
+							Next: nil,
+						},
+					},
+				},
+			},
+			want: &ListNode{
+				Val: 1,
+				Next: &ListNode{
+					Val: 1,
+					Next: &ListNode{
+						Val: 2,
+						Next: &ListNode{
+							Val: 3,
+							Next: &ListNode{
+								Val: 4,
+								Next: &ListNode{
+									Val:  4,
+									Next: nil,
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+		{
+			name: "Test",
+			args: args{
+				list1: nil,
+				list2: nil,
+			},
+			want: nil,
+		},
+		{
+			name: "Test",
+			args: args{
+				list1: nil,
+				list2: &ListNode{
+					Val:  0,
+					Next: nil,
+				},
+			},
+			want: &ListNode{
+				Val:  0,
+				Next: nil,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := mergeTwoLists(tt.args.list1, tt.args.list2); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("mergeTwoLists() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
