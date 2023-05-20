ac19284ee728635c44fe3142355ab7ea4fd8d6d0
chenshaorui <617672431@qq.com>
2023-05-20 10:19:42 +0800

    feat(problem-46): optimize unit test

--- Problem-46/Solution-1/main_test.go
+++ Problem-46/Solution-1/main_test.go
@@ -2,8 +2,47 @@ package Solution_1
 
 import (
 	"reflect"
+	"sort"
 	"testing"
 )
+
+type Results [][]int
+
+var _ sort.Interface = (*Results)(nil)
+
+func (results Results) Len() int {
+	return len(results)
+}
+
+func (results Results) Swap(i, j int) {
+	results[i], results[j] = results[j], results[i]
+}
+
+func (results Results) Less(i, j int) bool {
+	len1, len2 := len(results[i]), len(results[j])
+
+	if len1 < len2 {
+		return true
+	} else if len1 > len2 {
+		return false
+	} else {
+		for k := 0; k < len1; k++ {
+			if results[i][k] < results[j][k] {
+				return true
+			} else if results[i][k] > results[j][k] {
+				return false
+			}
+		}
+	}
+
+	return false
+}
+
+func sortResults(results [][]int) [][]int {
+	sort.Sort(Results(results))
+
+	return results
+}
 
 func Test_permute(t *testing.T) {
 	type args struct {
@@ -50,7 +89,7 @@ func Test_permute(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := permute(tt.args.nums); !reflect.DeepEqual(got, tt.want) {
+			if got := permute(tt.args.nums); !reflect.DeepEqual(sortResults(got), sortResults(tt.want)) {
 				t.Errorf("permute() = %v, want %v", got, tt.want)
 			}
 		})
